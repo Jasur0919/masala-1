29fb12de6f0db1a0318429ff1d67368618bd39fb
Jasur0919 <jasurjohn1111@gmail.com>
2024-07-14 16:37:27 +0500

    misol

---
+++ main.js
@@ -0,0 +1,30 @@
+//  1-ml
+
+// const n = 2
+// const obj = {a:2, b:3, c: 4}
+// for(let key in obj){
+//     obj[key] *= n
+// }
+
+// console.log(obj);
+
+//  2-ml
+
+// function countWordA(str){
+//     let words = str.split(' ')
+//     let count = 0
+//     for(let word of words ){
+//         if(word.includes("a") || word.includes("A")){
+//             count++
+//         }
+
+//     }
+//     return count
+// }
+// let text = "Ali vali okan "
+// console.log(countWordA(text));
+
+
+//  3-ml
+
+
